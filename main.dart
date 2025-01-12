217656c069cfc0f73f815887c3caed31648da271
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2025-01-12 17:35:59 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -20,33 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// Notifications Page
-class NotificationsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 2,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.redAccent,
-                child: Text(
-                  'O',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              title: Text('Oliver'),
-              subtitle: Text('Sample text notifications go here'),
-              trailing: Icon(Icons.clear, color: Colors.grey),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}

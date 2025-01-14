9598062c2b50c7bc2b84f7ef1c4c7966341c500b
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2025-01-14 12:07:34 +0530

    Update common.dart

--- common.dart
+++ common.dart
@@ -3,6 +3,7 @@ import 'package:geolocator/geolocator.dart';
 class Common {
 
   static Position? currentPosition;
+  static String? username;
   static List<Map<String, String>> contacts = [];
 
 }

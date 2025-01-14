573454b8109e4d19a211021a6c5c46d36480a788
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2025-01-14 10:52:23 +0530

    Create loginpage.dart

---
+++ loginpage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:freefall/homepage.dart';
+import 'dbhelper.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final DatabaseHelper _dbHelper = DatabaseHelper();
+
+  void _login() async {
+    final username = _usernameController.text.trim();
+
+    if (username.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter a username.')),
+      );
+      return;
+    }
+
+    final userExists = await _dbHelper.loginUser(username);
+
+    if (userExists) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Welcome back, $username!')),
+      );
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User does not exist.')),
+      );
+    }
+  }
+
+  void _register() async {
+    final username = _usernameController.text.trim();
+
+    if (username.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter a username.')),
+      );
+      return;
+    }
+
+    try {
+      await _dbHelper.addUser(username);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User registered successfully!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Username already exists.')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: _register,
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
